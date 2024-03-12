516781d4cf76682d0f7a4013828c3714459d9f5a
Corto Dufour <corto.dufour-fakhouri@hetic.net>
2024-03-12 16:55:33 +0100

    Import React

---
+++ src/Test.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+function Test() {
+  return (
+    <div className="Test">
+      <div className="Test">
+        <h1>Saucisse</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Test;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import JaaJTest from "./Test";
+
+ReactDOM.render(<JaaJTest />, document.getElementById("root"));
